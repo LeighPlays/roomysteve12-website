7b1cac73aad85134140a8c19d64c0c18c190faa8
LeighPlays <54193494+LeighPlays@users.noreply.github.com>
2020-12-19 21:20:57 +0800

    Add files via upload

---
+++ scripts/remove.js
@@ -0,0 +1,3 @@
+        $(document).ready(function(){
+            $('body').find('img[src$="https://cdn.000webhost.com/000webhost/logo/footer-powered-by-000webhost-white2.png"]').parent().closest('a').closest('div').remove();
+        });
